d46c889c827480db9468c96957448b3179d74d95
Andrew Kornder <40525838+andrewkornder@users.noreply.github.com>
2022-11-16 14:32:57 -0500

    .

--- downloading_music2/download_from_title/__init__.py
+++ downloading_music2/download_from_title/__init__.py
@@ -1,10 +1,10 @@
 from youtube_search import YoutubeSearch as Search
-from os import system, listdir, rename, remove, path, name as operating_sys
-from tkinter import Tk, Canvas, Entry, END, NW
+from os import system, listdir, remove, path, name as operating_sys
+from tkinter import Tk, Canvas, Entry, END, NW, Button
 from requests import get
 from PIL import Image, ImageTk
 from threading import Thread
-from youtube_dl import YoutubeDL as yt
+from youtube_dl import YoutubeDL as Yt
 from youtube_dl.utils import DownloadError
 from shutil import move
 import eyed3
@@ -12,17 +12,39 @@ from eyed3.id3.frames import ImageFrame
 
 
 class Downloader:
-    def __init__(self, text,k=None, w=1200, h=800, langs='en,ja',
-                 destination='C:/Users/Administrator/OneDrive/Desktop/folders/music',
-                 translator=None):
+    langs = ['en', 'ja']
+    options = {
+        'writethumbnail': True,
+        'quiet': True,
+        'nocheckcertificate': operating_sys == 'posix',
+        'writesubtitles': True,
+        'subtitleslangs': ['en', 'ja'],
+        'outtmpl': f'/%(id)s.%(ext)s',
+        'ffmpeg_location': '/Users/akornder25/Downloads/',
+        'postprocessors': [
+            {'key': 'FFmpegExtractAudio',
+             'preferredcodec': 'mp3'},
+            {'key': 'FFmpegMetadata'},
+            {'key': 'EmbedThumbnail'},
+        ]
+    }
+    destination = 'C:/Users/Administrator/OneDrive/Desktop/folders/music'
+
+    read = '../to_download'
+    temp = '../temp'
+    translator = '../translator'
+    past = '../past_saves'
+
+    def __init__(self, text=None, first_k=None, w=1200, h=800, langs=None,
+                 destination=None, translator=None, options=None, auto=False):
+
         self.i = 0
+        self.auto_select = auto
 
-        self.queries = (text if '\n' in text else open(text, encoding='UTF-8').read()).split('\n')
-        self.translate = (lambda *args: args) if translator is None else translator
+        self.translate = (lambda *args: args) if translator is None else \
+            (translator if translator is not True else self.get_translator(self.translator))
 
-        if k:
-            self.queries = self.queries[:k]
-        self.langs = langs.split(',')
+        self.langs = langs.split(',') if langs else self.langs
 
         self.w, self.h = w, h - 100
         self.length = 3
@@ -31,38 +53,88 @@ class Downloader:
         self.data = {}
         self.current = {}
         self.images = {}
-        self.destination = destination.replace('~', path.expanduser('~'))
+
+        if destination:
+            self.destination = destination.replace('~', path.expanduser('~'))
+
+        if options is not None:
+            self.options = options
+        else:
+            self.options['outtmpl'] = self.destination + self.options['outtmpl']
 
         self.font = ('Niagara Bold', 10)
 
         self.root = Tk()
         self.root.geometry(f'{self.w}x{h}')
 
-        self.canvas = Canvas(self.root, width=self.w, height=self.h)
-        self.canvas.grid(row=0, column=0)
+        self.canvas = Canvas(self.root, width=self.w, height=self.h, bg='white')
+        self.canvas.grid(row=0, column=0, columnspan=3)
 
         self.input = Entry(self.root)
-        self.input.grid(row=1, column=0)
+        self.input.grid(row=1, column=1)
+
+        self.toggler = Button(self.root, text='pause?', command=self.toggle_pause)
+        self.toggler.grid(row=1, column=2)
+
+        if text is None:
+            self.queries = open(self.read, encoding='UTF-8').read().split('\n')
+        elif type(text) == list:
+            self.queries = text
+        else:
+            self.queries = open(text.replace('~', path.expanduser('~')), encoding='UTF-8').read().split('\n')
+
+        if first_k:
+            self.queries = self.queries[:first_k]
+
+        self.paused = len(self.queries) == 0
 
         self.canvas.bind('<ButtonRelease-1>', lambda e: self.select(e.y // self.ih))
         self.canvas.bind('<ButtonPress-3>', self.back)
-        self.root.bind('<Return>', self.enter)
+        self.root.bind('<Return>', lambda *_: self.enter() if not self.paused else self.get_query())
+
+    def toggle_pause(self, *_):
+        self.paused = not self.paused
+
+        if self.paused:
+            self.draw_queries()
+        else:
+            self.load_next()
+
+    def draw_queries(self):
+        self.canvas.delete('all')
+
+        x, y = self.w / 2, 0
+        for q in self.queries:
+            y += 80
+            self.canvas.create_text(x, y, text=q, font=self.font)
+
+    def get_query(self, *_):
+        if not self.paused:
+            return
+
+        self.queries.append(self.input.get())
+        self.draw_queries()
+        self.input.delete(0, END)
 
     def load_next(self):
+        if self.paused:
+            return
+
         if self.i == len(self.queries):
             self.root.destroy()
+            self.root.update()
+
             self.finalize()
             return
 
         self.canvas.delete('all')
 
         self.current = Search(self.queries[self.i], max_results=self.length).to_dict()
-
         self.canvas.create_text(self.w / 2, self.ih * self.length + 25, text=self.queries[self.i], font=self.font)
 
         self.images[self.i] = []
         for j, video in enumerate(self.current):
-            name = f'../temp/{video["id"]}.jpg'
+            name = f'{self.temp}/{video["id"]}.jpg'
 
             open(name, 'wb').write(get(video['thumbnails'][0]).content)
 
@@ -74,14 +146,15 @@ class Downloader:
             self.canvas.create_text(ic[0] / 2, (j + 0.5) * self.ih, font=self.font,
                                     text=f"[{video['duration']}] \"{video['title']}\" - {video['channel']}")
 
-        # self.select(0)
+        if self.auto_select:
+            self.select(0)
 
-    def enter(self, _):
+    def enter(self, *_):
         self.queries[self.i] = self.input.get()
         self.input.delete(0, END)
         self.load_next()
 
-    def back(self, _):
+    def back(self, *_):
         self.i -= 1
 
     def select(self, i):
@@ -96,26 +169,9 @@ class Downloader:
         self.i += 1
         self.load_next()
 
-    def finalize(self):
-        ids, songs, artists = list(zip(*self.data.values()))
-
-        songs = list(map(self.sanitize, songs))
-
-        system('youtube-dl --rm-cache-dir --quiet')
-
-        y = yt({
-                'writethumbnail': True,
-                'quiet': True,
-                'writesubtitles': True,
-                'subtitleslangs': self.langs,
-                'outtmpl': f'{self.destination}/%(id)s.%(ext)s',
-                'postprocessors': [
-                    {'key': 'FFmpegExtractAudio',
-                     'preferredcodec': 'mp3'},
-                    {'key': 'FFmpegMetadata'},
-                    {'key': 'EmbedThumbnail'},
-                ]
-        })
+    @staticmethod
+    def download(options, x):
+        y = Yt(options)
 
         def dl(inputs):
             failed = []
@@ -129,29 +185,42 @@ class Downloader:
                     system('youtube-dl --rm-cache-dir --quiet')
             return failed
 
-        t = dl(ids)
-        while t:
-            print(f'{len(t)} downloads failed: restarting')
-            t = dl(t)
+        retry = dl(x)
+        while retry:
+            print(f'{len(retry)} downloads failed: restarting')
+            retry = dl(retry)
+
+    def finalize(self):
+        ids, songs, artists = list(zip(*self.data.values()))
+
+        songs = list(map(self.sanitize, songs))
+
+        system('youtube-dl --rm-cache-dir --quiet')
+
+        # TODO: self.download(self.options, ids)
 
         print('finished downloading')
 
         def get_info(index):
-            (t, a), func = self.translate(songs[index], artists[index]), lambda x, b: x if x else b
-            print(f'\n{t}')
+            def func(a, b): return a if a else b
 
-            a = func(input(f'artist = {artists[index]}?\n > '), a)
-            for style in (f'{a}: ', f'{a} - ', f' - {a}', a, f' {a} '):
-                t = t.replace(style, '')
+            (current_title, current_artist) = self.translate(songs[index], artists[index])
+            print(f'\n{current_title}')
 
-            t = func(input(f'title = {t}?\n > '), t)
-            print(f'using {t} by {a}')
+            current_artist = func(input(f'artist = {current_artist}?\n > '), current_artist)
+            for style in (f'{current_artist}: ', f'{current_artist} - ', f' - {current_artist}',
+                          current_artist, f' {current_artist} '):
+                current_title = current_title.replace(style, '')
 
-            return t, a
+            current_title = func(input(f'title = {current_title}?\n > '), current_title)
+            print(f'using {current_title} by {current_artist}')
+
+            return current_title, current_artist
 
         files = [x for x in listdir(self.destination) if path.isfile(self.destination + '/' + x)]
 
-        print('\n'.join(f'{a:<20}{b}' for a, b in zip((lambda l: (files[:l // 2], files[l//2:]))(len(files)))), '', sep='\n')
+        print('\n'.join(f'{a:<20}{b}' for a, b in zip(*(lambda l: (files[:l // 2], files[l // 2:]))(len(files)))), '',
+              sep='\n')
 
         for i, url in enumerate(ids):
             src = f'{self.destination}/{url}'
@@ -162,37 +231,56 @@ class Downloader:
             title, artist = get_info(i)
 
             tags = eyed3.load(src + '.mp3')
-            tags.tag.images.set(ImageFrame.FRONT_COVER, open(f'../temp/{url}.jpg', 'rb').read(), 'image/jpeg')
+            tags.tag.images.set(ImageFrame.FRONT_COVER, open(f'{self.temp}/{url}.jpg', 'rb').read(), 'image/jpeg')
             tags.tag.artist, tags.tag.title = artist, title
             tags.tag.save(version=eyed3.id3.ID3_V2_3)
 
-            rename(src + '.mp3', f'{self.destination}/{title}.mp3')
+            move(src + '.mp3', f'{self.destination}/mp3s/{title}.mp3')
             for lang in self.langs:
-                f = f'.{lang}.vtt'
-                if (url + f) in files:
-                    move(src + f, f'{self.destination}/subtitles/{title}{f}')
+                extension = f'.{lang}.vtt'
+                if (url + extension) in files:
+                    move(src + extension, f'{self.destination}/subtitles/{title}{extension}')
                 else:
                     print(f'no subtitles found for "{title}" with langs {lang}')
 
-            move(f'../temp/{url}.jpg', f'{self.destination}/thumbnails/{title}.jpg')
+            move(f'{self.temp}/{url}.jpg', f'{self.destination}/thumbnails/{title}.jpg')
 
         system(f'{"open" if operating_sys == "posix" else "start"} {self.destination}')
 
-        for file in listdir('./temp'):
-            remove(f'../temp/{file}')
+        for temp in listdir(self.temp):
+            remove(f'{self.temp}/{temp}')
 
+        open(self.past, 'a').write('\n'.join(f'{a}|{b}' for a, b in zip(ids, self.queries)))
         exit()
 
     def run(self):
-        self.load_next()
+        if not self.paused:
+            self.load_next()
+
         self.root.mainloop()
+        return self.data
 
     @staticmethod
     def sanitize(s):
         return ''.join(x for x in s if x not in '<>/\\"\':|?*')
 
+    @classmethod
+    def restore(cls, options=None):
+        cls.download(options if options else cls.options, [a[:10] for a in open(cls.past).read().split('\n')])
+
+    @staticmethod
+    def get_translator(file):
+        t0, t1, t2 = zip(
+            *[(lambda x: (x[0].strip(), *x[1].split(' | ')))(a.split(' : ')) for a in open(file).read().split('\n')])
+        t = dict(zip(t0, zip(t1, t2)))
+        return lambda x, n: (x, n) if x not in t else (lambda y, a: y if y[1] else (y[0], a))(t[x], n)
+
 
 if __name__ == '__main__':
-    t = dict(zip('Mao Abe／阿部真央 - まだいけます|幼女戦記 ED  Youjo Senki Ending 「FULL」 - Los! Los! Los! -  Tanya Degurechaff (悠木 碧)|【Ado】”罪と罰  Crime & Punishment 歌いました|Persona 4 Dancing All Night Opening Theme|砂の惑星  ハチ(cover) - Eve|太陽系デスコ  ナユタン星人(cover) - Eve|Flamingo Instrumental|Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai ED [Part Section]「Fukashigi no Carte」|Jake Chudnow - Moon Men (Instrumental)|jump man 93 - bruh|[KanjiRomajiEnglish] Hyouka-Madoromi no Yakusoku (Ending 1) まどろみの約束|[Hyouka] OP 2, Full Version - Mikansei Stride|Pax - Gee (Disco Remix)|PSYQUI - Fly to the moon feat. 中村さんそ|PSYQUI - Start Up feat. Such|PSYQUI - ヒステリックナイトガール feat. Such (android52 Edit)|android 52 - romance|android52 - super anime groove 3d world|Super Mario World Game Over LoFi Hip Hop Remix|Nanidato (ナニダト) - SUPER RISER! (Remake)|惑星ループ - Eve  feat.ナユタン星人'.split('|'), zip('まだいけます|Los! Los! Los!|罪と罰|Dancing All Night|砂の惑星|太陽系デスコ|Flamingo|Fukashigi no Carte|Moon Men|bruh|まどろみの約束|Mikansei Stride|Gee|Fly to the moon feat. 中村さんそ|Start Up feat. Such|ヒステリックナイトガール feat. Such (android52 edit)|romance|super anime groove 3d world|Super Mario World Game Over|SUPER RISER!|惑星ループ feat. ナユタン星人|'.split('|'), '阿部真央||Ado||Eve|Eve|||Jake Chudnow|jump man 93|||Pax|PSYQUI|PSYQUI|PSYQUI|android52|android52||ナニダト|Eve'.split('|'))))
-    f = lambda x, n: (x, n) if x not in t else (lambda y, a: y if y[1] else (y[0], a))(t[x], n)
-    Downloader(r'E:\code\downloading_music2\to_download', translator=f).run()
+    file = '~/Documents/GitHub/code/downloading_music2/to_download' if operating_sys == 'posix' else \
+        'E:/code/downloading_music2/to_download'
+
+    folder = '~/Documents/GitHub/folders/music/' if operating_sys == 'posix' else \
+        '~/OneDrive/Desktop/folders/music'
+
+    ids = Downloader([], destination=folder, translator=True).run()
